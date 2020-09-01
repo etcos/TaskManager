67ec87b954a932e33dcc524273b3b76c232cef32
Evgeniy Panov <e.panov@fundcount.com>
2020-09-01 16:26:58 +0300

    + TM-9 add TestTask2 class

---
+++ src/main/java/org/taskmanager/entity/CTestTask2.java
@@ -0,0 +1,13 @@
+package org.taskmanager.entity;
+
+public class CTestTask2 {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
