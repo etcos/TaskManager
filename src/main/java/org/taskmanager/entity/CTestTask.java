1ea8a513beb42a4d5c8d9ed9234071d518643af7
Evgeniy Panov <e.panov@fundcount.com>
2020-09-01 16:20:03 +0300

    + TM-6 add test task class

---
+++ src/main/java/org/taskmanager/entity/CTestTask.java
@@ -0,0 +1,13 @@
+package org.taskmanager.entity;
+
+public class CTestTask {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
